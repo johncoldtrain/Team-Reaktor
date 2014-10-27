35e1738450d98c81dff2ce2d54f2df93add57100
Alex Romanillos <alexromanillos@Alexs-MacBook-Pro.local>
2014-10-27 21:51:00 +0100

    Install devise, add user views and controllers, added user_id key in statuses

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Added when installed the devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # In production, host: should be set to the actual host of the application
+
 end
